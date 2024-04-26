5a2ff0d03a4d0bfe4f0864267c10acbcc0c0a5b4
penneShells <61021287+penneShells@users.noreply.github.com>
2024-04-26 13:34:23 +0100

    Update DataCollect_CV2.py

--- DataCollect_CV2.py
+++ DataCollect_CV2.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 def faceCollect(video, type, folder, neighbours):
     #Load the video using cv2 and assign the frame to the variable image
@@ -7,7 +8,7 @@ def faceCollect(video, type, folder, neighbours):
     count = 0
     #Load the Haar cascade to detect faces
     faceDetector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
+    print(success)
     while success:
         #Convert to gray to make facial detection easier
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -29,33 +30,25 @@ def faceCollect(video, type, folder, neighbours):
             counter = count + ((type - 1) * 1000)
 
             #Save to a dataset folder
-            cv2.imwrite(f"datasets/256_cv2/{folder}/frame%d.jpg" % counter, resized)
+            cv2.imwrite(f"datasets/256_cv2_test/{folder}/frame%d.jpg" % counter, resized)
         count += 1
         success, image = vidCap.read()
     print(f"Data collected from {video}")
 
 
 #Big function to run faceCollect on all videos.
-#Will probably end up automating the calling of this with iteration but there are bigger fish to fry right now
-#Also the dummy arguments are required for use in the GUI so just ignore them
+#Dummy arguments are required for use in the GUI so just ignore them
 def collectAll(dummy, dummy2):
     print("Collecting data - This may take a while")
-
-    faceCollect("videos/video_archie.mp4", 1, "archie", 8)
-    faceCollect("videos/video_archie_2.mp4", 2, "archie", 8)
-    faceCollect("videos/video_archie_3.mp4", 3, "archie", 8)
-    faceCollect("videos/video_archie_4.mp4", 4, "archie", 8)
-
-    faceCollect("videos/video_alfie.mp4", 1, "alfie", 4)
-    faceCollect("videos/video_alfie_2.mp4", 2, "alfie", 4)
-    faceCollect("videos/video_alfie_3.mp4", 3, "alfie", 7)
-
-    faceCollect("videos/video_keiram.mp4", 1, "keiran", 4)
-    faceCollect("videos/video_keiran_2.mp4", 2, "keiran", 4)
-
-    faceCollect("videos/video_josh.mp4", 1, "josh", 4)
-
-    faceCollect("videos/video_ethan_1.mp4", 1, "ethan", 2)
+    videos = [video for video in os.listdir('videos')]
+    for video in videos:
+        print(video)
+        name = video.split("_")[1]
+        faceCollect(f"videos/{video}", int(video[len(video) - 1]), name, 4)
 
     print("Data collection Achieved \n")
     print("You may wish to check the generated images for irregularities")
+
+
+if __name__ == "__main__":
+    collectAll(None, None)
